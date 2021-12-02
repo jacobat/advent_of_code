fc1416d89c39d46e47486bd506315615c7b00c3a
Jacob Atzen <jatzen@gmail.com>
2021-12-02 16:20:41 +0100

    Simplify day1 a bit

--- 2021/1/day1.exs
+++ 2021/1/day1.exs
@@ -2,36 +2,21 @@ defmodule Day1 do
   # def input, do: File.read!("test")
   def input, do: File.read!("input")
 
-  def input_lines do
+  def count_increases(window_size) do
     input()
     |> String.split("\n")
-    |> Enum.reject(&(String.length(&1) == 0))
     |> Enum.map(&String.to_integer/1)
-  end
-
-  def chunks(size), do: input_lines() |> Enum.chunk_every(size, 1, :discard)
-
-  def count_increases do
-    chunks(2)
-    |> count_increases()
-  end
-
-  def count_window_increases do
-    chunks(3)
+    |> Enum.chunk_every(window_size, 1, :discard)
     |> Enum.map(&Enum.sum/1)
     |> Enum.chunk_every(2, 1, :discard)
-    |> count_increases()
-  end
-
-  def count_increases(list) do
-    list
     |> Enum.filter(fn [a,b] -> a < b end)
     |> Enum.count
   end
 
   def run do
-    %{ increases: count_increases(),
-      window_increases: count_window_increases()
+    %{
+      increases: count_increases(1),
+      window_increases: count_increases(3)
     }
   end
 end
