8d9cf48851aa7240c84b36d36f3af48c667dcd8d
Jacob Atzen <jatzen@gmail.com>
2021-12-20 10:56:31 +0100

    Optimizing Day 20

--- 2021/20/day20.exs
+++ 2021/20/day20.exs
@@ -4,20 +4,38 @@ defmodule Day20 do
     # File.read!("test")
   end
 
+  def pixel_to_num(pixel) do
+    if pixel == ".", do: "0", else: "1"
+  end
+
   def parse(input) do
     [img_enh_alg|img] = input
     |> String.split("\n", trim: true)
 
-    image = Enum.with_index(img, fn line, row ->
-      line
-      |> String.codepoints
-      |> Enum.with_index(fn pixel, col ->
-        %{{col, row} => pixel}
-      end)
+    image =
+      img
+      |> Enum.with_index(fn line, row ->
+        line
+        |> String.codepoints
+        |> Enum.map(&pixel_to_num/1)
+        |> Enum.with_index(fn pixel, col ->
+          %{{col, row} => pixel}
+        end)
       |> Enum.reduce(&Map.merge/2)
     end)
     |> Enum.reduce(&Map.merge/2)
-    {img_enh_alg, image}
+
+    algorithm = img_enh_alg
+                |> String.codepoints
+                |> Enum.with_index(fn pixel, index ->
+                  binary = Integer.to_string(index, 2)
+                  |> String.pad_leading(9, "0")
+                  |> String.codepoints
+                  %{ binary => pixel |> pixel_to_num }
+                end)
+                |> Enum.reduce(&Map.merge/2)
+
+    {fn x -> Map.get(algorithm, x) end, image}
   end
 
   def dimensions(image, elem) do
@@ -34,34 +52,35 @@ defmodule Day20 do
   end
 
   def window_to_number(window) do
-    Enum.map(window, fn pixel ->
-      if pixel == ".", do: "0", else: 1
-    end)
+    window
     |> Enum.join
     |> String.to_integer(2)
   end
 
-  def enhance(image, {x, y}, img_enh_alg, default) do
+  def neighbours(image, {x, y}, default) do
     for y0 <- (y - 1)..(y + 1), x0 <- (x - 1)..(x + 1) do
       Map.get(image, {x0, y0}, default)
     end
-    |> window_to_number
-    |> then(fn num -> String.at(img_enh_alg, num) end)
+  end
+
+  def enhance(image, {x, y}, img_enh_alg, default) do
+    neighbours(image, {x, y}, default)
+    |> img_enh_alg.()
   end
 
   def update_default(default, img_enh_alg) do
     default
     |> List.duplicate(9)
-    |> window_to_number
-    |> then(fn num -> String.at(img_enh_alg, num) end)
+    |> img_enh_alg.()
   end
 
   def enhance({image, default}, img_enh_alg) do
     [{x_min, x_max}, {y_min, y_max}] = dimensions(image)
-    new_image = for y <- (y_min - 2)..(y_max + 2), x <- (x_min - 2)..(x_max + 2) do
-      %{{x + 2, y + 2} => enhance(image, {x, y}, img_enh_alg, default)}
-    end
-    |> Enum.reduce(&Map.merge/2)
+    new_image = for y <- (y_min - 2)..(y_max + 2),
+      x <- (x_min - 2)..(x_max + 2),
+      into: %{} do
+        {{x, y}, enhance(image, {x, y}, img_enh_alg, default)}
+      end
     { new_image, default |> update_default(img_enh_alg) }
   end
 
@@ -69,7 +88,7 @@ defmodule Day20 do
     [{x_min, x_max}, {y_min, y_max}] = dimensions(image)
     for y <- (y_min - 2)..(y_max + 2) do
       for x <- (x_min - 2)..(x_max + 2) do
-        Map.get(image, {x, y}, default)
+        if Map.get(image, {x, y}, default) == "1", do: "#", else: "."
       end
       |> Enum.join
       |> IO.puts
@@ -80,12 +99,12 @@ defmodule Day20 do
   def count_lit_pixels({image, _}) do
     Map.values(image)
     |> Enum.frequencies
-    |> Map.get("#")
+    |> Map.get("1")
   end
 
   def run(input, iterations) do
     {img_enh_alg, image} = input |> parse
-    {image, "."}
+    {image, "0"}
     |> Stream.iterate(&enhance(&1, img_enh_alg))
     |> Stream.drop(iterations)
     |> Enum.take(1)
