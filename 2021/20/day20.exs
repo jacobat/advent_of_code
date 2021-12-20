21ceee3e11f7bc4de08c67ade1b34d06493fc0db
Jacob Atzen <jatzen@gmail.com>
2021-12-20 12:14:37 +0100

    Before chunk

--- 2021/20/day20.exs
+++ 2021/20/day20.exs
@@ -8,32 +8,43 @@ defmodule Day20 do
     if pixel == ".", do: "0", else: "1"
   end
 
+  def string_to_int(s) do
+    s
+    |> String.replace(".", "0")
+    |> String.replace("#", "1")
+  end
+
   def parse(input) do
     [img_enh_alg|img] = input
     |> String.split("\n", trim: true)
 
-    image =
-      img
-      |> Enum.with_index(fn line, row ->
-        line
-        |> String.codepoints
-        |> Enum.map(&pixel_to_num/1)
-        |> Enum.with_index(fn pixel, col ->
-          %{{col, row} => pixel}
-        end)
-      |> Enum.reduce(&Map.merge/2)
-    end)
-    |> Enum.reduce(&Map.merge/2)
+    image = img
+            |> Enum.map(&string_to_int/1)
+    #   img
+    #   |> Enum.with_index(fn line, row ->
+    #     line
+    #     |> String.codepoints
+    #     |> Enum.map(&pixel_to_num/1)
+    #     |> Enum.with_index(fn pixel, col ->
+    #       %{{col, row} => pixel}
+    #     end)
+    #   |> Enum.reduce(&Map.merge/2)
+    # end)
+    # |> Enum.reduce(&Map.merge/2)
 
     algorithm = img_enh_alg
                 |> String.codepoints
                 |> Enum.with_index(fn pixel, index ->
                   binary = Integer.to_string(index, 2)
                   |> String.pad_leading(9, "0")
-                  |> String.codepoints
+                  # |> String.codepoints
+                  # |> Enum.chunk_every(3)
+                  # |> Enum.map(&Enum.join/1)
                   %{ binary => pixel |> pixel_to_num }
                 end)
                 |> Enum.reduce(&Map.merge/2)
+                # |> IO.inspect(label: "Alg")
+
 
     {fn x -> Map.get(algorithm, x) end, image}
   end
@@ -46,8 +57,8 @@ defmodule Day20 do
 
   def dimensions(image) do
     [
-      dimensions(image, 0),
-      dimensions(image, 1)
+      {0, (List.first(image) |> String.length) - 1},
+      {0, Enum.count(image) - 1}
     ]
   end
 
@@ -57,30 +68,64 @@ defmodule Day20 do
     |> String.to_integer(2)
   end
 
-  def neighbours(image, {x, y}, default) do
-    for y0 <- (y - 1)..(y + 1), x0 <- (x - 1)..(x + 1) do
-      Map.get(image, {x0, y0}, default)
-    end
-  end
+  def neighbours(lines, {x, y}) do
+    # IO.inspect({x, y}, label: "Image for n #{x}, #{y}")
+    slices = Enum.map(lines, fn line ->
+      # line = bench(fn -> Enum.at(image, y0 + 2) end, "at")
+      # Enum.at(image, y0 + 2)
+             # |> IO.inspect(label: "Line")
 
-  def enhance(image, {x, y}, img_enh_alg, default) do
-    neighbours(image, {x, y}, default)
-    |> img_enh_alg.()
+      # bench(fn -> String.slice(line, x + 2 - 1, 3) end, "slice")# |> IO.inspect(label: "Neighbours")
+      String.slice(line, x + 2 - 1, 3)
+      # Map.get(image, {x0, y0}, default)
+    end)
+    # bench(fn -> Enum.join(slices) end, "join")
+    Enum.join(slices)
   end
 
   def update_default(default, img_enh_alg) do
     default
-    |> List.duplicate(9)
+    |> String.duplicate(9)
+    # |> List.duplicate(3)
     |> img_enh_alg.()
+  end
+
+  def enhance_pixel(lines, {x, y}, img_enh_alg) do
+    # ns = bench(fn -> neighbours(lines, {x, y}) end, "neighbours #{x}, #{y}")
+    ns = neighbours(lines, {x, y})
+    # |> IO.inspect(label: "Neighbours")
+    img_enh_alg.(ns)
+  end
+
+  def grow_image(image, default) do
+    default_line = String.duplicate(default, (Enum.at(image, 0) |> String.length) + 4)
+    [ default_line, default_line ] ++
+      Enum.map(image, fn line ->
+        String.duplicate(default, 2) <> line <> String.duplicate(default, 2)
+      end) ++
+        [ default_line, default_line ]
+  end
+
+  def bench(fun, label) do
+    t0 = DateTime.now!("Etc/UTC")
+    result = fun.()
+    t1 = DateTime.now!("Etc/UTC")
+    DateTime.diff(t1, t0, :microsecond)
+    |> IO.inspect(label: label)
+    result
   end
 
   def enhance({image, default}, img_enh_alg) do
     [{x_min, x_max}, {y_min, y_max}] = dimensions(image)
-    new_image = for y <- (y_min - 2)..(y_max + 2),
-      x <- (x_min - 2)..(x_max + 2),
-      into: %{} do
-        {{x, y}, enhance(image, {x, y}, img_enh_alg, default)}
+
+    # padded_image = bench(fn -> grow_image(image, default) end, "Grow image")
+    padded_image = grow_image(image, default)
+    new_image = for y <- (y_min - 1)..(y_max + 1) do
+      lines = Enum.slice(padded_image, y - 1 + 2, 3)
+      for x <- (x_min - 1)..(x_max + 1), into: "" do
+        enhance_pixel(lines, {x, y}, img_enh_alg)
       end
+    end
     { new_image, default |> update_default(img_enh_alg) }
   end
 
@@ -97,19 +142,26 @@ defmodule Day20 do
   end
 
   def count_lit_pixels({image, _}) do
-    Map.values(image)
-    |> Enum.frequencies
-    |> Map.get("1")
+    Enum.map(image, fn line ->
+      String.codepoints(line)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum
+    end)
+    |> Enum.sum
+    # Map.values(image)
+    # |> Enum.frequencies
+    # |> Map.get("1")
   end
 
   def run(input, iterations) do
     {img_enh_alg, image} = input |> parse
     {image, "0"}
+    # |> enhance(img_enh_alg)
     |> Stream.iterate(&enhance(&1, img_enh_alg))
     |> Stream.drop(iterations)
     |> Enum.take(1)
     |> List.first
-    # |> display
+    # # |> display
     |> count_lit_pixels
   end
 
